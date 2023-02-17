582994a5eeb9de7ae7c3caca89a3a836ae1865d4
Kingchenkong <wikeyno@gmail.com>
2023-02-16 20:07:11 -0800

    [Modify] to function, and output file: hello, to exexute.

--- Proj-Hello_World/2023_0216-Hello_World.cpp
+++ Proj-Hello_World/2023_0216-Hello_World.cpp
@@ -8,36 +8,24 @@ void functionNameLog(string);
 void consoleHello();
 void consoleDate1(int, int, int);
 void consoleDate2(int, int, int);
+void consoleASCII();
+void consoleInputNumber();
 
 int main(void)
 {
-    // std::cout << "Hello world, C++. 2023 0216" << std::endl;
     consoleHello();
 
     int year = 2023;
     int month = 02;
     int date = 16;
 
-    // std::cout << "Today date is " << year << "/" << month << "/" << date << "" << std::endl;
     consoleDate1(year, month, date);
 
-    // std::cout << "Year is " << year;
-    // std::cout << ", Month is " << month;
-    // std::cout << ", day is " << date << std::endl;
     consoleDate2(year, month, date);
 
-    char char_a = 'a';
-    int ascii_a = char_a;
-    char char_A = 'A';
-    int ascii_A = char_A;
+    consoleASCII();
 
-    std::cout << char_a << ", ASCII code is " << ascii_a << std::endl;
-    std::cout << char_A << ", ASCII code is " << ascii_A << std::endl;
-
-    int inputNumber;
-    std::cout << "Input a Number: ";
-    std::cin >> inputNumber;
-    std::cout << "input is " << inputNumber << std::endl;
+    consoleInputNumber();
 
     system("pause");
     return 0;
@@ -70,5 +58,30 @@ void consoleDate2(int y, int m, int d)
     std::cout << "Year is " << y;
     std::cout << ", Month is " << m;
     std::cout << ", day is " << d << std::endl;
+    return;
+}
+
+void consoleASCII()
+{
+    functionNameLog("consoleASCII");
+    char char_a = 'a';
+    int ascii_a = char_a;
+    char char_A = 'A';
+    int ascii_A = char_A;
+
+    std::cout << char_a << ", ASCII code is " << ascii_a << std::endl;
+    std::cout << char_A << ", ASCII code is " << ascii_A << std::endl;
+    return;
+}
+
+void consoleInputNumber()
+{
+    functionNameLog("consoleInputNumber");
+
+    int inputNumber;
+    std::cout << "Input a Number: ";
+    std::cin >> inputNumber;
+    std::cout << "input is " << inputNumber << std::endl;
+
     return;
 }
