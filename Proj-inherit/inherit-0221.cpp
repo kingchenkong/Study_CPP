858d6e260029a782f3399433cd60adddefd624d1
Kingchenkong <wikeyno@gmail.com>
2023-02-21 00:32:49 -0800

    [Add] inherit-0221, init

---
+++ Proj-inherit/inherit-0221.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+// declaration: class
+
+// declaration: funciton
+void consoleDivider()
+{
+    cout << " - - - - - - - - - - - - - - - - - - - - " << endl;
+}
+
+// declaration: field variables, hint: must initialize...
+
+// main
+int main()
+{
+
+    consoleDivider();
+    system("pause");
+    return 0;
+}
