a021b8433743e38c7127a3167fbe0477b7fce581
Kingchenkong <wikeyno@gmail.com>
2023-03-01 22:32:20 -0800

    [Add] Proj-Modular and CMake practice.

---
+++ Proj-Modular/build/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/function.cpp.o"
+  "CMakeFiles/MyApp.dir/function.cpp.o.d"
+  "CMakeFiles/MyApp.dir/main.cpp.o"
+  "CMakeFiles/MyApp.dir/main.cpp.o.d"
+  "MyApp"
+  "MyApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
